3b198800559d7a896d49c5c267dea823872e0185
zhangxin123456 <398101150@qq.com>
2017-07-31 08:25:00 +0800

    第三次

--- app/src/main/java/test/bwie/jiyun/com/ins7566/ipandatv/module/pandalive/bean/PandaLiveBean.java
+++ app/src/main/java/test/bwie/jiyun/com/ins7566/ipandatv/module/pandalive/bean/PandaLiveBean.java
@@ -13,7 +13,6 @@ public class PandaLiveBean implements Serializable{
      * live : [{"title":"成都基地高清精切线路","brief":"翻身、吃饭、睡觉、喝奶、打闹、攀爬\u2026\u2026这里是成都大熊猫繁育研究基地，24小时高清直播大熊猫生活实况的地方。成年园、幼年园、幼儿园、母子园、一号别墅，11路直播信号28个摄像头，让你零距离了解国宝们的日常起居。","image":"http://p1.img.cctvpic.com/photoAlbum/page/performance/img/2016/1/5/1451989464985_497.jpg","id":"ipanda","isshow":"true","url":"http://live.ipanda.com/stream/"}]
      * bookmark : {"multiple":[{"title":"多视角直播","url":"http://www.ipanda.com/kehuduan/PAGE14501769230331752/PAGE14501787896813312/index.json","order":"1"}],"watchTalk":[{"title":"边看边聊","url":"zhiboye_chat","order":"1"}]}
      */
-
     private BookmarkBean bookmark;
     private List<LiveBean> live;
 
