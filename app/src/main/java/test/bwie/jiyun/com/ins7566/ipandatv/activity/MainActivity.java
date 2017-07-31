ed68def63fc1479aadbc090ed43d61248560bebe
zhangxin123456 <398101150@qq.com>
2017-07-31 08:12:43 +0800

    第三次

--- app/src/main/java/test/bwie/jiyun/com/ins7566/ipandatv/activity/MainActivity.java
+++ app/src/main/java/test/bwie/jiyun/com/ins7566/ipandatv/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package test.bwie.jiyun.com.ins7566.ipandatv.activity;
 
 import android.os.Bundle;
+import android.os.Process;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,8 +17,11 @@ import test.bwie.jiyun.com.ins7566.ipandatv.App;
 import test.bwie.jiyun.com.ins7566.ipandatv.R;
 import test.bwie.jiyun.com.ins7566.ipandatv.base.BaseActivity;
 import test.bwie.jiyun.com.ins7566.ipandatv.base.BaseFragment;
+import test.bwie.jiyun.com.ins7566.ipandatv.module.home.fragment.HomeFragment;
 import test.bwie.jiyun.com.ins7566.ipandatv.module.livechina.fragment.LiveChinaFragment;
 import test.bwie.jiyun.com.ins7566.ipandatv.module.pandabroadcast.fragment.PandaBroadcastFragment;
+import test.bwie.jiyun.com.ins7566.ipandatv.module.pandaculture.fragment.PandaCultureFragment;
+import test.bwie.jiyun.com.ins7566.ipandatv.module.pandalive.fragment.PandadirectFragment;
 import test.bwie.jiyun.com.ins7566.ipandatv.widget.manger.FragmentBuilder;
 
 public class MainActivity extends BaseActivity {
@@ -48,7 +52,8 @@ public class MainActivity extends BaseActivity {
     ImageView hudongImg;
     private long lastTime;//上一次点击back键的时间毫秒数
     public static final int HOMETYPE = 1;
-    private  FragmentManager fragmentManager;
+    private FragmentManager fragmentManager;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -63,7 +68,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initData() {
 //        ConfigFragment.getInstance().init().start(HomeFragment.class).build();
-//        ConfigFragment.getInstanca().start(HomeFragment.class).builder();
+//        ConfigFragment.getInsanca().start(HomeFragment.class).builder();
         FragmentBuilder.getInstance().start(HomeFragment.class, R.id.FrameLayout).builder();
     }
 
@@ -90,12 +95,12 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.main_broadcast_btn:
                 initView();
-                showTitle("熊猫观察",0);
-                FragmentBuilder.getInstance().start(PandaBroadcastFragment.class,R.id.FrameLayout).builder();
+                showTitle("熊猫观察", 0);
+                FragmentBuilder.getInstance().start(PandaBroadcastFragment.class, R.id.FrameLayout).builder();
                 break;
             case R.id.main_china_btn:
-                showTitle("直播中国",0);
-                FragmentBuilder.getInstance().start(LiveChinaFragment.class,R.id.FrameLayout).builder();
+                showTitle("直播中国", 0);
+                FragmentBuilder.getInstance().start(LiveChinaFragment.class, R.id.FrameLayout).builder();
                 break;
 
         }
@@ -147,11 +152,11 @@ public class MainActivity extends BaseActivity {
             System.exit(0);
 
 
-        }else{
+        } else {
             if (fragmentManager.getBackStackEntryCount() > 1) {
                 fragmentManager.popBackStackImmediate();//执行弹栈，立马执行
                 //否则记录得到位于栈顶的类名字
-                String simpleName = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount()-1 ).getName();
+                String simpleName = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName();
                 //记录做标记，标记为上一个Fragment,点击back键刷新lastFragment
                 App.lastFragment = (BaseFragment) fragmentManager.findFragmentByTag(simpleName);
             }
@@ -165,7 +170,7 @@ public class MainActivity extends BaseActivity {
         Process.killProcess(Process.myPid());//获取pid
         System.exit(0);
 //        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
-        super.onDestroy();
+
     }
 
     //隐藏下面的RadioGroup
@@ -175,20 +180,5 @@ public class MainActivity extends BaseActivity {
 
     public void setMainRadioGroup(RadioGroup mainRadioGroup) {
         FrameLayoutContentGroup = mainRadioGroup;
-    }
-
-    @OnClick({R.id.personImg, R.id.hudongImg})
-    public void onClicked(View view) {
-        switch (view.getId()) {
-            case R.id.personImg:
-
-                Intent intent1 = new Intent(this,MyselfActivity.class);
-                startActivity(intent1);
-                break;
-            case R.id.hudongImg:
-                Intent intent = new Intent(this, HudongActivity.class);
-                startActivity(intent);
-                break;
-        }
     }
 }
